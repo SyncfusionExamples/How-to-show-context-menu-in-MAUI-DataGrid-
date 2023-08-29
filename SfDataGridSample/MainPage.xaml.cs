74d110a2bdd3062b53d5603e0a769da37a520c1a
NirmalKumarYuvaraj <nirmalkumar.yuvaraj@syncfusion.com>
2023-08-29 17:35:57 +0530

    Added Sample for the KB and updated README.md

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.Maui.DataGrid;
+
+namespace SfDataGridSample
+{
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+#if WINDOWS || MACCATALYST
+        dataGrid.CellRightTapped += DataGrid_CellRightTapped;
+#endif
+
+    }
+
+    private void DataGrid_CellRightTapped(object sender, DataGridCellRightTappedEventArgs e)
+    {
+        viewModel.currentColumnName = dataGrid.Columns[e.RowColumnIndex.ColumnIndex].MappingName;
+    }
+
+        
+}
+}
